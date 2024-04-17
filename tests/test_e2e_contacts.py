90c764095d20ce393051e1899b3413ac0dd038c2
Vitalii Bielimov <vbit2000@gmail.com>
2024-04-17 15:17:04 +0300

    test_e2e_contacts completed

--- tests/test_e2e_contacts.py
+++ tests/test_e2e_contacts.py
@@ -1,41 +1,45 @@
-# from datetime import date
-from unittest.mock import Mock, patch
+from unittest.mock import patch, AsyncMock
 
 import pytest
 
 from src.auth.services import auth_service
 
 
-def test_get_contacts(client, get_token):
+def test_get_contacts(client, get_token, monkeypatch):
     with patch.object(auth_service, 'cache') as redis_mock:
         redis_mock.get.return_value = None
-        token = get_token
-        headers = {"Authorization": f"Bearer {token}"}
+        monkeypatch.setattr("fastapi_limiter.FastAPILimiter.redis", AsyncMock())
+        monkeypatch.setattr("fastapi_limiter.FastAPILimiter.identifier", AsyncMock())
+        monkeypatch.setattr("fastapi_limiter.FastAPILimiter.http_callback", AsyncMock())
+        headers = {"Authorization": f"Bearer {get_token}"}
         response = client.get("api/contacts", headers=headers)
         assert response.status_code == 200, response.text
         data = response.json()
         assert len(data) == 0
 
 
-# def test_create_contact(client, get_token, monkeypatch):
-#     with patch.object(auth_service, 'cache') as redis_mock:
-#         redis_mock.get.return_value = None
-#         token = get_token
-#         headers = {"Authorization": f"Bearer {token}"}
-#         response = client.post("api/contacts", headers=headers, json={
-#             "first_name": "Jane",
-#             "last_name": "Smith",
-#             "email": "jane.smith@example.com",
-#             "phone_number": "9876543210",
-#             "date_of_birth": "1985-08-15",
-#             "additional_data": "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-#         })
-#         assert response.status_code == 201, response.text
-#         data = response.json()
-#         assert "id" in data
-#         assert data["first_name"] == "Jane"
-#         assert data["last_name"] == "Smith"
-#         assert data["email"] == "jane.smith@example.com"
-#         assert data["phone_number"] == "9876543210"
-#         assert data["date_of_birth"] == "1985-08-15"
-#         assert data["additional_data"] == "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+def test_create_contact(client, get_token, monkeypatch):
+    with patch.object(auth_service, 'cache') as redis_mock:
+        redis_mock.get.return_value = None
+        monkeypatch.setattr("fastapi_limiter.FastAPILimiter.redis", AsyncMock())
+        monkeypatch.setattr("fastapi_limiter.FastAPILimiter.identifier", AsyncMock())
+        monkeypatch.setattr("fastapi_limiter.FastAPILimiter.http_callback", AsyncMock())
+        token = get_token
+        headers = {"Authorization": f"Bearer {token}"}
+        response = client.post("api/contacts", headers=headers, json={
+            "first_name": "Jane",
+            "last_name": "Smith",
+            "email": "jane.smith@example.com",
+            "phone_number": "9876543210",
+            "date_of_birth": "1985-08-15",
+            "additional_data": "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        })
+        assert response.status_code == 201, response.text
+        data = response.json()
+        assert "id" in data
+        assert data["first_name"] == "Jane"
+        assert data["last_name"] == "Smith"
+        assert data["email"] == "jane.smith@example.com"
+        assert data["phone_number"] == "9876543210"
+        assert data["date_of_birth"] == "1985-08-15"
+        assert data["additional_data"] == "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
